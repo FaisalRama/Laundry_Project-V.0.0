b32ac90407a2a9f673163e9221ab83e01aeb704f
Xall_Tzy <frjgaming56@gmail.com>
2022-02-01 19:32:56 +0700

    2nd commit, member migration and model

---
+++ app/Models/member.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class member extends Model
+{
+    public $incrementing = true;
+    protected $table = 'tb_member';
+    protected $fillable = ['nama', 
+                            'alamat', 
+                            'jenis_kelamin', 
+                            'tlp'];
+}
